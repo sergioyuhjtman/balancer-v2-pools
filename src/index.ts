b4e94ff8fa71bda63c26eb8d97879c9852f161f0
Sergio Yuhjtman <39177270+sergioyuhjtman@users.noreply.github.com>
2021-09-07 19:10:48 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import StablePool from "./pools/stable";
 import * as StableMath from "./pools/stable/math";
 import WeightedPool from "./pools/weighted";
 import * as WeightedMath from "./pools/weighted/math";
+import * as LinearMath from "./pools/linear/math";
 
-export { StablePool, StableMath, WeightedPool, WeightedMath };
+export { StablePool, StableMath, WeightedPool, WeightedMath, LinearMath };
